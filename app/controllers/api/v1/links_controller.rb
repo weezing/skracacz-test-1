015be51ffd70550d730356497b87396829ded5b5
jarek <weezing@protonmail.com>
2020-11-01 23:38:44 +0100

    Use strong params

--- app/controllers/api/v1/links_controller.rb
+++ app/controllers/api/v1/links_controller.rb
@@ -1,9 +1,12 @@
 class Api::V1::LinksController < Api::V1::BaseController
   def create
-    link = Link.create!(
-      original_link: jsonapi_params[:original_link],
-      slug: SecureRandom.hex(10)
-    )
+    link = Link.create!(create_params.merge(slug: SecureRandom.hex(10)))
     render json: link.to_json, status: :created
+  end
+
+  private
+
+  def create_params
+    jsonapi_params.permit(:original_link)
   end
 end
